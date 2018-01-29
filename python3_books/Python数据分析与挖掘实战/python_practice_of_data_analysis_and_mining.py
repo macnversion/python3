ebfa920799802f7879322880a305c99ef1371c72
macnversion <chuan.ma@qq.com>
2018-01-30 07:51:13 +0800

    update

--- python3_books/Python数据分析与挖掘实战/python_practice_of_data_analysis_and_mining.py
+++ python3_books/Python数据分析与挖掘实战/python_practice_of_data_analysis_and_mining.py
@@ -3,7 +3,10 @@
 # %%
 import os
 import pandas as pd
+import scipy as sp
+from scipy.interpolate import lagrange
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 # %% 相关参数设定
 plt.rcParams['font.sans-serif'] = ['SimHei'] # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False # 用来正常显示负号
@@ -35,8 +38,44 @@ p = 1.0*dish_profit.cumsum()/dish_profit.sum()
 p.plot(color='r', secondary_y=True, style='-o', linewidth=2)
 plt.ylabel(u'盈利（占比）')
 
+# %% 连续属性离散化
+discretization_data = pd.read_excel('./dataset/Python数据分析与挖掘实战/discretization_data.xls')
+data = discretization_data[u'肝气郁结证型系数'].copy()
+k = 4
+
+d1 = pd.cut(data, k, labels=range(4)) # 等宽离散化
+
+w = [1.0*i/k for i in range(k+1)]
+w = data.describe(percentiles = w)[4:4+k+1]
+w[0] = w[0]*(1-1e-10)
+d2 = pd.cut(data, w, labels=range(k)) # 等频率离散化
+
+
+kmodel = KMeans(n_clusters = k, n_jobs = 4) # 建立模型，n_jobs是并行数
+kmodel.fit(data.values.reshape((len(data), 1)))
+c = pd.DataFrame(kmodel.cluster_centers_).sort_values(0)
+w = pd.rolling_mean(c, 2).iloc[1:]
+w = [0] + list(w[0]) + data.max()
+d3 = pd.cut(data, w, labels=range(k))
+
 # %% 相关性分析
 catering_sale = pd.read_excel('./dataset/Python数据分析与挖掘实战/catering_sale_all.xls', index_col=u'日期')
 catering_sale.corr()
 catering_sale.corr()[u'百合酱蒸凤爪']
 catering_sale[u'百合酱蒸凤爪'].corr(catering_sale[u'翡翠蒸香茜饺'])
+
+# %% lagrange插值法
+catering_sale = pd.read_excel('./dataset/Python数据分析与挖掘实战/catering_sale.xls')
+catering_sale[u'销量'][(catering_sale[u'销量']<400)|(catering_sale[u'销量']>5000)] = None
+
+def ployinterp_column(s, n, k=5):
+    y = s[list(range(n-k, n)) + list(range(n+1, n+1+k))]
+    y = y[y.notnull()]
+    return lagrange(y.index, list(y))(n)
+
+for i in catering_sale.columns:
+    for j in range(len(catering_sale)):
+        if (catering_sale[i].isnull())[j]:
+            catering_sale[i][j] = ployinterp_column(catering_sale[i], j)
+
+# %%
