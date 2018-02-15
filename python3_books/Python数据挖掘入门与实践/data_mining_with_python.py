dc486eb09e108b1011721b9544273d8a9cc599d2
macnversion <chuan.ma@qq.com>
2018-02-15 16:16:28 +0800

    start practice

--- python3_books/Python数据挖掘入门与实践/data_mining_with_python.py
+++ python3_books/Python数据挖掘入门与实践/data_mining_with_python.py
@@ -10,7 +10,8 @@ from collections import defaultdict
 sorted(os.listdir('./dataset/python数据挖掘入门与实践'))
 # %%
 X = np.loadtxt('./dataset/python数据挖掘入门与实践/affinity_dataset.txt')
-features = ['面包', '牛奶', '奶酪', '苹果', '香蕉']
+n_sample, n_features = X.shape
+features = ['面包', '牛奶', '奶酪', '苹果', '香蕉'] # 数据集的列对应的水果
 
 # 以购买苹果为例，计算购买苹果的cases
 num_apple_purchases = 0
@@ -38,8 +39,43 @@ confidence = rule_valid/num_apple_purchases
 valid_rules = defaultdict(int)
 invalid_rules = defaultdict(int)
 num_occurances = defaultdict(int)
-
+# 计算所有可能的组合
 for sample in X:
-    for premise in range(4):
+    for premise in range(n_features):
         if sample[premise] == 0:
             continue
+        num_occurances[premise] += 1
+        for conclusion in range(n_features):
+            if sample[premise] == conclusion:
+                continue
+            if sample[conclusion] == 1:
+                valid_rules[(premise, conclusion)] += 1
+            else:
+                invalid_rules[(premise, conclusion)] += 1
+support = valid_rules
+confidence = defaultdict(float)
+for premise, conclusion in valid_rules.keys():
+    confidence[(premise, conclusion)] = valid_rules[(premise, conclusion)]/num_occurances[premise]
+
+for primise, conclusion in confidence:
+    primise_name = features[primise]
+    conclusion_name = features[conclusion]
+    print('Rule: If a persion buy {0} they will also buy {1}'.format(primise_name, conclusion_name))
+    print('- Confidence:{0:.3f}'.format(confidence[(primise, conclusion)]))
+    print('- Support:{0}'.format(support[(primise, conclusion)]))
+    print('\n')
+
+def print_rule(premise, conclusion, support, confidence, features):
+    premise_name = features[premise]
+    conclusion_name = features[conclusion]
+    print('Rule: If a person buy {0} they will also buy {1}'.format(premise_name, conclusion_name))
+    print('- Support: {0}'.format(support[(premise, conclusion)]))
+    print('- Confidence: {0:.3f}'.format(confidence[(premise, conclusion)]))
+    print('\n')
+
+# 排序找出最佳的规则
+from operator import itemgetter
+sorted_support = sorted(support.items(), key=itemgetter(1), reverse=True)
+for index in range(n_features):
+    print('Rules #{0}'.format(index+1))
+    (premise, conclusion) = sorted_support()
