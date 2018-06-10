a071f401b9f2935db10a301437d9420b6bd58a8d
macnversion <chuan.ma@qq.com>
2018-06-10 16:41:21 +0800

    new

---
+++ python3_books/python数据科学手册/python_data_science_handbook.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*
+
+# %%
+import os
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+
+# %% 计算代码的执行时间
+# %timeit L = [n ** 2 for n in range(100)]
+
+# %% numpy
+
