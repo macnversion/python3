2b58d8d44f9b420151a7aae0cfae029e10993570
macnversion <chuan.ma@qq.com>
2018-06-11 23:04:02 +0800

    update

--- python3_books/python数据科学手册/python_data_science_handbook.py
+++ python3_books/python数据科学手册/python_data_science_handbook.py
@@ -10,4 +10,29 @@ from pandas import DataFrame, Series
 # %timeit L = [n ** 2 for n in range(100)]
 
 # %% numpy
+# numpy中的可以直接应用的数组
+np.zeros(10 ,dtype=int) # 创建长度为10的零数组
+np.ones((3, 5), dtype=float) # 创建3✖*5的浮点数组
+np.full((3, 5), 3.14) # 创建3*5的浮点数组，数值全部都是3.14
+np.arange(0, 20, 2) # 0开始，20结束，步长为2
+np.linspace(0, 1, 5) # 0到1之间均匀分布的5个数值
+np.random.randint(0, 10, (3, 3)) # 创建一个3*3的随机数组
+np.eye(3)
 
+np.random.seed(0)
+x1 = np.random.randint(10, size=6)
+x2 = np.random.randint(10, size=(3, 4))
+x3 = np.random.randint(10, size=(3, 4, 5))
+print('x3 ndim:', x3.ndim, '\n')
+print('x3 shape:', x3.shape, '\n')
+print('x3 size:', x3.size, '\n')
+print('x3 dtype:', x3.dtype, '\n')
+
+'''
+numpy中的数组是固定类型的，插入一个浮点数到整型的数组中，浮点数将会被截短成为整型
+'''
+print('x1=', x1)
+x1[0] = 3.14
+print('x1=', x1)
+
+# 获取短数组： x[start: stop: step]
