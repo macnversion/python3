f9b195541c6ee5971558f7ab2223436efae47b87
macnversion <chuan.ma@qq.com>
2019-09-13 18:24:44 +0800

    update

--- python3_books/web_scraping_with_python/web_scraping_with_python.py
+++ python3_books/web_scraping_with_python/web_scraping_with_python.py
@@ -2,6 +2,9 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import re
+import datetime
+import random
 
 url1 = 'http://pythonscraping.com/pages/page1.html'
 url2 = 'http://www.pythonscraping.com/pages/warandpeace.html'
@@ -41,3 +44,46 @@ for child in bsObj.find('table', {'id': 'giftList'}).children:
 for child in bsObj.find('table', {'id': 'giftList'}).descendants:
     pass
     print(child)
+
+for sibling in bsObj.find('table', {'id': 'giftList'}).tr.next_siblings:
+    print(sibling)
+
+images = bsObj.find_all('img', {'src': re.compile('\\.\\.\\/img\\/gifts\\/img*\\.jpg')})
+for image in images:
+    print(image)
+
+
+url4 = 'https://en.wikipedia.org/wiki/Eric_Idle'
+url5 = 'https://en.wikipedia.org/wiki/Kevin_Bacon'
+html = urlopen(url4)
+bsObj = BeautifulSoup(html)
+for link in bsObj.find_all('a'):
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
+
+
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen('https://en.wikipedia.org' + articleUrl)
+    bsObj = BeautifulSoup(html)
+    return bsObj.find('div', {'id': 'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+
+
+links = getLinks('/wiki/Kevin_Bacon')
+while len(links)>0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+    print(newArticle)
+    links = getLinks(newArticle)
+
+
+pages = set()
+def getLinks2(pageUrl):
+    global pages
+    html = urlopen('https://en.wikipedia.org' + pageUrl)
+    bsObj = BeautifulSoup(html)
+    for link in bsObj.find_all('a', href=re.compile('^(/wiki/)')):
+        if link.attrs['href'] not in pages:
+            newPage = link.attrs['href']
+            print(newPage)
+            pages.add(newPage)
+            getLinks2(newPage)
