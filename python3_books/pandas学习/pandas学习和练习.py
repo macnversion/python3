20cb7bf50346091070a17f9d50a1d317b7bed2bc
macnversion <chuan.ma@qq.com>
2021-04-25 00:03:33 +0800

    update

--- python3_books/pandas学习/pandas学习和练习.py
+++ python3_books/pandas学习/pandas学习和练习.py
@@ -15,6 +15,23 @@ import os
 # %%
 df = pd.read_csv('./dataset/pandas/learn_pandas.csv')
 print('df的列名称包含:\n{0}'.format(df.columns))
-df = df[df.columns[0:6]]
+df = df[df.columns[0:7]]
 print('df.shape is:{0}'.format(df.shape))
 print('精简后的df的列名称包含:\n{0}'.format(df.columns))
+
+# 一些基础的函数
+df.head()
+df.tail()
+df.info()
+df.describe()
+
+df_demo = df[['Height', 'Weight']]
+df_demo.max()
+
+df['School'].unique() # 返回去重的结果
+df['School'].nunique() # 返回不含重复项的数量
+df['School'].value_counts() # 类似groupby后的count计数操作
+
+# 再起构造一个数据
+df_demo = df[['Gender', 'Transfer', 'Name']]
+df_demo.drop_duplicates(['Gender', 'Transfer'])
